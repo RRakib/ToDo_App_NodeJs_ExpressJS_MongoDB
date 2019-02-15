501da1f9e58a415c112e6b2e2b5e00f7777fe3ea
Rakib Uddin <16103030@iubat.edu>
2019-02-15 14:57:34 +0600

    Added POST and DELETE

--- Public/todo.js
+++ Public/todo.js
@@ -1,17 +1,47 @@
-window.addEventListener("load" , () => {
-    let formSubmit = document.querySelector("#formSubmit");
-    formSubmit.addEventListener("submit", (e) => {
-        e.preventDefault();
+// $(document).ready(function(){
+
+//     $('form').on('submit', function(){
+  
+//         var item = $('form input');
+//         var todo = {item: item.val()};
+  
+//         $.ajax({
+//           type: 'POST',
+//           url: '/todo',
+//           data: todo,
+//           success: function(data){
+//             //do something with the data via front-end framework
+//             location.reload();
+//           }
+//         });
+  
+//         return false;
+  
+//     });
+  
+//     $('li').on('click', function(){
+//         var item = $(this).text().replace(/ /g, "-");
+//         $.ajax({
+//           type: 'DELETE',
+//           url: '/todo/' + item,
+//           success: function(data){
+//             //do something with the data via front-end framework
+//             location.reload();
+//           }
+//         });
+//     });
+  
+//   });
+
+
+
+    document.addEventListener("loadeddata" , function() {
+    document.querySelector("#formSubmit").addEventListener("submit",function() {
+        let xhr = new XMLHttpRequest();
         let inputTodo = document.querySelector("#inputTodo").value;
         let data = {item : inputTodo};
-        let xhr = new XMLHttpRequest();
-        xhr.open("POST" , "/todo" , true);
-        xhr.onload = (data) => {
-            if(xhr.status === 200){
-                location.reload();
-            }
-        }
-        xhr.send();
+        xhr.open("POST" , "/todo" ,  true);
+        xhr.send(data);
+        window.location.href("/todo");
     })
-
 })
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,6 +1,6 @@
 let bodyParser = require("body-parser")
 
-data = [
+let data = [
     {item: "Start Coding"},
     {item: "Sleep Some More"},
     {item: "Again Start Coding"}
@@ -9,18 +9,20 @@ let urlencodedParser = bodyParser.urlencoded({ extended : false})
 
 
 
-module.exports = (app) => {
+module.exports = function(app) {
     app.get("/todo" , (req, res) => {
         res.render("todo", {
             todo : data
         })
     })
-    app.post("/todo" ,urlencodedParser,  (req, res) => {
+    app.post("/todo" ,urlencodedParser,  function(req, res) {
+        console.log(req.body)
         data.push(req.body);
-        res.json(data);
+        res.json(data)
     })
 
-    app.delete("/todo" , (rea, res) => {
-
+    app.delete("/todo/:item" , (req, res) => {
+        data = data.filter((todo) => {return todo.item.replace(/ /g, "-") !== req.params.item})
+        res.json(data)
     })
 }
